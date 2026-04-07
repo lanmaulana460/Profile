355576da142d45ff3d78d02439026f3166b74ee4
lanmaulana460 <lanmaulana460@gmail.com>
2026-04-07 23:00:30 +0700

    update js/app.js

--- js/app.js
+++ js/app.js
@@ -19,4 +19,36 @@ document.onkeydown = function(e) {
     if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
         return false;
     }
+    const texts = [
+"✨ Welcome to Maulana STORe",
+"📦 Open Panel Bot WhatsApp & Telegram",
+"🤖 Open Sewa Bot WhatsApp",
+"📱 Open Nokos All Aplikasi"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+function type(){
+    if(count === texts.length){
+        count = 0;
+    }
+
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById("typing").textContent = letter;
+
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+        setTimeout(type,1500);
+    }else{
+        setTimeout(type,40);
+    }
+}
+
+type();
 };
